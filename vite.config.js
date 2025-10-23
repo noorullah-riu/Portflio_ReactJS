678c49b0b9113da62ec5e93191bc3988c9b2554a
noorullah-riu <noorullah.riu@gmail.com>
2025-10-23 13:55:56 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+//  "homepage": "https://noorullah-riu.github.io/Portflio_ReactJS/",
 export default defineConfig({
   plugins: [react()],
+  base: '/Portflio_ReactJS/', // Add this line
 })
